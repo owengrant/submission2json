afe782b6080f36fbefd794264adcb785696c4dde
Owen Grant <owen.dalmon.grant@gmail.com>
2020-05-31 12:37:20 -0400

    add little test

--- src/main/java/com/geoideas/ds/Submission2Json.java
+++ src/main/java/com/geoideas/ds/Submission2Json.java
@@ -13,8 +13,17 @@ public class Submission2Json {
     public static void main(String[] args) throws IOException, XMLStreamException {
 //        var xform = Files.readAllLines(Path.of("demo1.xml")).stream().reduce("", String::concat);
 //        var form = Files.readAllLines(Path.of("form.json")).stream().reduce("", String::concat);
-//        var converter = new JsonConverter(new JSONObject(form), xform.trim());
-//        System.out.println(converter.convert());
+//        var count = 100;
+//        var total = 0;
+//        for(var i = 0; i < count; i++) {
+//            var start = System.currentTimeMillis();
+//            var converter = new JsonConverter(new JSONObject(form), xform.trim());
+//            converter.convert();
+//            var end = System.currentTimeMillis() - start;
+//            total += end;
+//            System.out.println("time : "+end);
+//        }
+//        System.out.println("Average : "+total/count);
     }
 
 }
