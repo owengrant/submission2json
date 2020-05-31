bf7532dcccbecdca110ebc049dd3047c347847ff
Owen Grant <owen.dalmon.grant@gmail.com>
2020-05-31 17:49:09 -0400

    improve repeat group performance; fix data corruption when repeat group child fields are missing

--- src/main/java/com/geoideas/ds/JsonConverter.java
+++ src/main/java/com/geoideas/ds/JsonConverter.java
@@ -11,6 +11,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class JsonConverter {
+    public static final String COUNT = "_count";
+    long start = 0;
+    long total = 0;
     public static final String SURVEY = "survey";
     public static final String ROOT = "data";
     public static final String PREFIX = "/";
@@ -27,9 +30,18 @@ public class JsonConverter {
     private JSONObject submission;
     private Map<String, JSONObject> formMap;
 
+    private class RepeatParent {
+        String name;
+        JSONArray parent;
+        public RepeatParent(String name, JSONArray parent) {
+            this.name = name;
+            this.parent = parent;
+        }
+    }
+
     public JsonConverter(JSONObject form, String xform) {
         this.form = form;
-        this.xform = xform;
+        this.xform = xform.trim();
         this.submission = new JSONObject();
         var survey = this.form.getJSONObject(SURVEY);
         this.formMap = survey.keySet().stream()
@@ -46,16 +58,26 @@ public class JsonConverter {
         var currentField = "";
         while(reader.hasNext()) {
             var element = reader.nextEvent();
+//            if(element.isStartElement()) {
+//                var tag = element.asStartElement();
+//                System.out.println(tag.getName().getLocalPart());
+//                if(tag.getName().getLocalPart().equals("age")) ageCount++;
+//            }
             if(element.isStartElement()) {
-              var tag = element.asStartElement();
-              var name = tag.getName().getLocalPart();
-              if(name.equals(ROOT)) {
-                  submission.put(XFORM_ID_STRING, tag.getAttributeByName(new QName(ID)).getValue());
-                  var version = tag.getAttributeByName(new QName(VERSION));
-                  if(version != null) submission.put(XFORM_VERSION, version.getValue());
-              }
-              else currentField = name;
-              addIfRepeat(name, submission);
+                var tag = element.asStartElement();
+                var name = tag.getName().getLocalPart();
+                if(name.equals(ROOT)) {
+                    submission.put(XFORM_ID_STRING, tag.getAttributeByName(new QName(ID)).getValue());
+                    var version = tag.getAttributeByName(new QName(VERSION));
+                    if(version != null) submission.put(XFORM_VERSION, version.getValue());
+                }
+                else currentField = name;
+                addIfRepeat(name, submission);
+                if(formMap.containsKey(name)) {
+                    var formField = formMap.get(name);
+                    var formFieldCount = formField.has(COUNT) ? formField.getInt(COUNT)+1 : 0;
+                    formField.put(COUNT, formFieldCount);
+                }
             }
             else if(element.isCharacters()) {
                 addField(currentField, element.asCharacters().getData());
@@ -92,30 +114,23 @@ public class JsonConverter {
             addToSubmission(submission, field, value);
         }
         else {
-            // place value in first repeat object for which the key does not exist
-            var repeat = firstRepeatParent(field);
-            JSONObject childSubmission = null;
-            var found = false;
-            for(var item: repeat) {
-                childSubmission = (JSONObject) item;
-                var formatterName = formatName(field.getString(NAME));
-                if(!childSubmission.has(formatterName)) {
-                    found = true;
-                    break;
-                }
-            }
-            // repeat is empty or a child with field was not found
-            if((childSubmission == null) || (childSubmission != null && !found)) {
-                var repeatName = firstRepeatParentName(field);
-                var repeatField = form.getJSONObject(SURVEY).getJSONObject(repeatName);
+            var repeatP = firstRepeatParent(field);
+            var repeat = repeatP.parent;
+            var repeatSize = repeat.length();
+            JSONObject childSubmission = repeatSize == 0
+                    ? null : repeat.getJSONObject(repeatSize - 1);
+            var childIndex = (repeatSize - 1);
+            var repeatField = form.getJSONObject(SURVEY).getJSONObject(repeatP.name);
+
+            var formFieldCount = formMap.get(fieldName).getInt(COUNT);
+            if((childSubmission == null) || (childSubmission != null && formFieldCount > childIndex)) {
                 childSubmission = new JSONObject();
-                if(repeatField.has(PARENT)) {
-                    var parentRepeat = firstRepeatParent(repeatField);
-                    childSubmission.put("_repeat_parent_index", parentRepeat.length());
-                }
                 repeat.put(childSubmission);
             }
-
+            if(repeatField.has(PARENT)) {
+                var parentRepeat = firstRepeatParent(repeatField).parent;
+                childSubmission.put("_repeat_parent_index", parentRepeat.length());
+            }
             addToSubmission(childSubmission, field, value);
         }
     }
@@ -137,18 +152,11 @@ public class JsonConverter {
         }
     }
 
-    private JSONArray firstRepeatParent(JSONObject field) {
+    private RepeatParent firstRepeatParent(JSONObject field) {
         var parentName = field.getString(PARENT);
         var parent = form.getJSONObject(SURVEY).getJSONObject(parentName);
-        if(isRepeat(parent)) return submission.getJSONArray(formatName(parentName));
+        if(isRepeat(parent)) return new RepeatParent(parentName, submission.getJSONArray(formatName(parentName)));
         else return firstRepeatParent(parent);
-    }
-
-    private String firstRepeatParentName(JSONObject field) {
-        var parentName = field.getString(PARENT);
-        var parent = form.getJSONObject(SURVEY).getJSONObject(parentName);
-        if(isRepeat(parent)) return parentName;
-        else return firstRepeatParentName(parent);
     }
 
     private boolean inOrIsRepeat(JSONObject json) {
