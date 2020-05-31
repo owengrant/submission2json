73d527ca3581e36f4fa9a218b69d39024f692ac6
Owen Grant <owen.dalmon.grant@gmail.com>
2020-05-31 17:48:08 -0400

    add small test for manual vs community

--- src/main/java/com/geoideas/ds/Submission2Json.java
+++ src/main/java/com/geoideas/ds/Submission2Json.java
@@ -1,6 +1,8 @@
 package com.geoideas.ds;
 
+import com.github.wnameless.json.flattener.JsonFlattener;
 import org.json.JSONObject;
+import org.json.XML;
 
 import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
@@ -10,20 +12,34 @@ import java.nio.file.Path;
 
 public class Submission2Json {
 
-    public static void main(String[] args) throws IOException, XMLStreamException {
+//    public static void main(String[] args) throws IOException, XMLStreamException {
 //        var xform = Files.readAllLines(Path.of("demo1.xml")).stream().reduce("", String::concat);
 //        var form = Files.readAllLines(Path.of("form.json")).stream().reduce("", String::concat);
 //        var count = 100;
 //        var total = 0;
 //        for(var i = 0; i < count; i++) {
 //            var start = System.currentTimeMillis();
-//            var converter = new JsonConverter(new JSONObject(form), xform.trim());
-//            converter.convert();
+//            var converter = new JsonConverter(new JSONObject(form), xform);
+//            var json = converter.convert();
 //            var end = System.currentTimeMillis() - start;
 //            total += end;
-//            System.out.println("time : "+end);
 //        }
 //        System.out.println("Average : "+total/count);
-    }
+//    }
+
+//        public static void main(String[] args) throws IOException, XMLStreamException {
+//        var xform = Files.readAllLines(Path.of("demo1.xml")).stream().reduce("", String::concat);
+//        var form = Files.readAllLines(Path.of("form.json")).stream().reduce("", String::concat);
+//        var count = 100;
+//        var total = 0;
+//        for(var i = 0; i < count; i++) {
+//            var start = System.currentTimeMillis();
+//            var json = XML.toJSONObject(xform);
+//            String jsonStr = JsonFlattener.flatten(json.toString());
+//            var end = System.currentTimeMillis() - start;
+//            total += end;
+//        }
+//        System.out.println("Average : "+total/count);
+//    }
 
 }
